aa30c392aba364e1ad9f1bf2da7f4994cbe9a7fc
u2 <zhangyaning1985@gmail.com>
2014-04-02 22:11:43 +0800

    焦点突

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui-1.10.3.min.js
-//= require js/bootstrap.js
+//= require bootstrap.js
 //= require turbolinks
 //= require plugins/metisMenu/jquery.metisMenu.js
 //= require sb-admin.js
