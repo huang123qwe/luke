57887939fa8ed351874a570ffb1a77a39e6daf37
u2 <zhangyaning1985@gmail.com>
2014-04-07 11:04:29 +0800

    left banner and right banner

--- app/models/doing.rb
+++ app/models/doing.rb
@@ -3,7 +3,10 @@ class Doing < ActiveRecord::Base
 
   validates_presence_of :position
 
+  scope :left_banner, -> { where(position: '左') }
+  scope :right_banner, -> { where(position: '右') }
+
   def self.positions
-    ["左下","右下"]
+    ["左下","右下","左","右"]
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,6 +36,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [140, 170]
   end
 
+  version :banner do
+    process :resize_to_fit => [421, 480]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
