9bac753f6fdc470552c377cbb01f855788b97f3a
u2 <zhangyaning1985@gmail.com>
2014-04-14 22:53:32 +0800

    fix assets problem

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Luke::Application.initialize!
-
- # Maps asset types to public directory.
-  ASSET_PUBLIC_DIRECTORIES = {
-    audio:      '/audios',
-    font:       '/fonts',
-    image:      '/images',
-    javascript: '/javascripts',
-    stylesheet: '/stylesheets',
-    video:      '/videos'
-  }
