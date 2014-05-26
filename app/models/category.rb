fcc465900ccaad757f778aad52e938f743d95160
u2 <zhangyaning1985@gmail.com>
2014-05-26 21:12:53 +0800

    before save

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   validates_uniqueness_of :name
 
-  before_update :filter_comma
+  before_save :filter_comma
 
   def filter_comma
     self.tags = self.tags.to_s.gsub(/，/,',')
