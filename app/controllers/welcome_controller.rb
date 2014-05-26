134dad1f9083ae9b240f8893a82ffc79445f96ca
u2 <zhangyaning1985@gmail.com>
2014-05-26 14:42:54 +0800

    a small fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    categories = Category.all.to_a || []
+    categories = Category.all.to_a
     @where_tags = categories.detect{|i| i.name == "去哪里" }.tags.split(',')
     @doing_tags = categories.detect{|i| i.name == "去干吗" }.tags.split(',')
     @hotes_tags = categories.detect{|i| i.name == "酒店" }.tags.split(',')
